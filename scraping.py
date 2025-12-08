2bf55073b58dc0bee5f1948172c77d18b29e06c3
Juliana Tocci <juliana.tocci@gmail.com>
2025-12-08 11:31:31 -0300

    feat: script para fazer o web scraping da página de filmes

---
+++ scraping.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.imdb.com/chart/top/"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/142.0.0.0 Safari/537.36"
+}
+
+response = requests.get(url, headers=headers)
+html = response.text
+soup = BeautifulSoup(html, "html.parser")
+
+filmes = []
+
+filmes_divs = soup.select("div.ipc-metadata-list-summary-item__c")
+
+for filme_div in filmes_divs:
+    titulo_tag = filme_div.select_one("h3.ipc-title__text")
+    titulo = titulo_tag.get_text(strip=True) if titulo_tag else "N/A"
+
+    ano_tag = filme_div.select_one("span.cli-title-metadata-item")
+    ano = ano_tag.get_text(strip=True) if ano_tag else "N/A"
+
+    nota_tag = filme_div.select_one("span.ipc-rating-star--rating")
+    nota = nota_tag.get_text(strip=True) if nota_tag else "N/A"
+
+    filmes.append({
+        "titulo": titulo,
+        "ano": ano,
+        "nota": nota
+    })
+
+for f in filmes[:5]:
+    print(f"{f['titulo']} ({f['ano']}) – Nota: {f['nota']}")
